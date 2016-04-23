6358670f148c81761cd0c022e1616836098c3c7a
ivandejanovic <idejanovic@gmail.com>
2016-04-23 20:15:46 +0200

    Added logger to MainServiceImpl.

--- src/main/java/com/quine/springdemo/service/MainServiceImpl.java
+++ src/main/java/com/quine/springdemo/service/MainServiceImpl.java
@@ -3,15 +3,20 @@
  */
 package com.quine.springdemo.service;
 
+import org.slf4j.LoggerFactory;
+
 /**
  * @author Ivan Dejanovic
  *
  * @version 1.0
  */
 public class MainServiceImpl implements MainService {
+    private final static org.slf4j.Logger logger = LoggerFactory.getLogger(MainServiceImpl.class);
 
     @Override
     public String retrieveMessage() {
+        logger.info("Invoking retrieve message.");
+        
         return "Service message";
     }
 
